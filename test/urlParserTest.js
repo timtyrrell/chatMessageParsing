7098535b42f9bb0e23b1d8e72f69350e8e9056a8
Tim Tyrrell <tim@tyrrell.io>
2015-03-01 19:51:45 -0600

    Add returned blank title test

--- test/urlParserTest.js
+++ test/urlParserTest.js
@@ -5,15 +5,20 @@ var sinon = require('sinon')
 
 describe('UrlParser', () => {
   let parser
+  let fetchStub
 
-  before(() => { parser = new UrlParser() })
+  before(() => {
+    parser = new UrlParser()
+    fetchStub = sinon.stub(parser, 'fetchTitles')
+  })
+  after(() => { fetchStub.restore() })
 
   describe('with valid input', () => {
-    before(() => {
-        sinon.stub(parser, 'fetchTitles').yields(null, [ 'NBC Olympics | Home of the 2016 Olympic Games in Rio','Google'])
-    })
-
     describe('and http in the url', () => {
+      before(() => {
+        fetchStub.yields(null, [ 'NBC Olympics | Home of the 2016 Olympic Games in Rio','Google'])
+      })
+
       it("returns an array of url's and titles", (done) => {
         parser.parse("Something; http://www.nbcolympics.com http://www.google.com", (res) => {
           res.should.deep.equal({
@@ -31,6 +36,10 @@ describe('UrlParser', () => {
     })
 
     describe('and without http in the url', () => {
+      before(() => {
+        fetchStub.yields(null, [ 'NBC Olympics | Home of the 2016 Olympic Games in Rio','Google'])
+      })
+
       it("returns an array of url's and titles", (done) => {
         parser.parse("Something; http://www.nbcolympics.com www.google.com", (res) => {
           res.should.deep.equal({
@@ -40,6 +49,27 @@ describe('UrlParser', () => {
                 "title": "NBC Olympics | Home of the 2016 Olympic Games in Rio"
               },
               {"url": "http://www.google.com", "title":"Google"}
+            ]
+          })
+          done()
+        })
+      })
+    })
+
+    describe('and without a title returned', () => {
+      before(() => {
+        fetchStub.yields(null, [ 'NBC Olympics | Home of the 2016 Olympic Games in Rio',''])
+      })
+
+      it("returns an array of url's and titles", (done) => {
+        parser.parse("Something; http://www.nbcolympics.com www.google.com", (res) => {
+          res.should.deep.equal({
+            "links": [
+              {
+                "url": "http://www.nbcolympics.com",
+                "title": "NBC Olympics | Home of the 2016 Olympic Games in Rio"
+              },
+              {"url": "http://www.google.com", "title":""}
             ]
           })
           done()
